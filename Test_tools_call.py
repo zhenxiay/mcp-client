a93d95caf7ce1240a5b1a8eab64d0a2fed884623
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2025-05-16 12:31:30 +0200

    Refactor MCPClient to change 'input_schema' to 'parameters' and add a new test file for client functionality

--- Test_tools_call.py
+++ Test_tools_call.py
@@ -6,8 +6,6 @@ from contextlib import AsyncExitStack
 from mcp import ClientSession, StdioServerParameters
 from mcp.client.stdio import stdio_client
 
-from anthropic import Anthropic
-from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()  # load environment variables from .env
@@ -17,8 +15,6 @@ class MCPClient:
         # Initialize session and client objects
         self.session: Optional[ClientSession] = None
         self.exit_stack = AsyncExitStack()
-        self.anthropic = Anthropic()
-        self.openai_agent = OpenAI()
 
     async def connect_to_server(self, server_script_path: str):
         """Connect to an MCP server
@@ -49,55 +45,15 @@ class MCPClient:
         tools = response.tools
         print("\nConnected to server with tools:", [tool.name for tool in tools])
 
-    async def process_query(self, query: str) -> str:
-        """Process a query using Claude and available tools"""
-        messages = [
-            {
-                "role": "user",
-                "content": query
-            }
-        ]
-
+    async def list_tools(self) -> str:
         response = await self.session.list_tools()
-        available_tools = [{ 
-            "name": tool.name,
-            "description": tool.description,
-            "type": "function",
-            "parameters": tool.inputSchema
-        } for tool in response.tools]
-
-        # Initial Open AI API call
-        response = self.openai_agent.responses.create(
-        model="gpt-4.1",
-        tools=available_tools,
-        input=messages
-        )
-
-        # Process response and handle tool calls
-        final_text = []
-
-        for tool_call in response.output:
-            if tool_call.type != "function_call":
-                final_text.append(response.output[0].content.text)
-
-            tool_name = tool_call.name
-            tool_args = json.loads(tool_call.arguments)
-
-            result = await self.session.call_tool(tool_name, tool_args)
-            final_text.append(f"[Calling tool {tool_name} with args {tool_args}]")
-            
-            messages.append({
-                "type": "function_call_output",
-                "call_id": tool_call.call_id,
-                "output": str(result)
-            })
-
-        return "\n".join(final_text), available_tools, response.output
+        tools = response.tools
+        return tools
 
     async def chat_loop(self):
         """Run an interactive chat loop"""
         print("\nMCP Client Started!")
-        print("Type your queries or 'quit' to exit.")
+        print("Type 'quit' to exit.")
 
         while True:
             query = input("\nQuery: ").strip()
@@ -105,23 +61,10 @@ class MCPClient:
             if query.lower() == 'quit':
                 break
                     
-            response, available_tools, output = await self.process_query(query)
-            print("\n" + response)
-            print(available_tools)
-            print(output)
-       
-        #while True:
-        #    try:
-        #        query = input("\nQuery: ").strip()
-        #        
-        #        if query.lower() == 'quit':
-        #            break
-        #            
-        #        response = await self.process_query(query)
-        #        print("\n" + response)
-        #            
-        #    except Exception as e:
-        #        print(f"\nError: {str(e)}")
+            tools = await self.list_tools()
+            tool_args = tools[0].inputSchema
+            print(tools)
+            print(tool_args)
     
     async def cleanup(self):
         """Clean up resources"""
